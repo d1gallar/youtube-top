d4e4d43ca82aad08b20cbbc0933c4b0edeb55cb0
David Gallardo <davidgallardo101@gmail.com>
2022-02-18 10:42:04 -0800

    Minor Server Change

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 
+const PORT = process.env.PORT || 8080;
+
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 8080;
 
 const channelRouter = require("./routes/Channels");
 const videoRouter = require("./routes/Videos");
@@ -15,18 +16,16 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.render(path.resolve(__dirname, "../client/build/", "index.html"));
+  res.render(path.resolve(__dirname, "../client/build/index.html"));
 });
 
 app.get("/channels", channelRouter);
 app.get("/videos", videoRouter);
 
-if (process.NODE_ENV === "production") {
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/public", "index.html"));
+});
 
-app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Listening at http://localhost:${PORT}`);
 });
